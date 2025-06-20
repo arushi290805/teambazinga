43e07b316b6858e1ab564e3bed4ff710a5edfecd
Arushi <alokk@LAPTOP-OSIS5GE5.>
2025-06-20 11:44:14 +0530

    using HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(<App />);
+  createRoot(rootElement).render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
 }
